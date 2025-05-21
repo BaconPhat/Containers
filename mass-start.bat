722bc8fcadf2fce16c7cc2596b458c03c4c2a89c
PJ Guibord <pj@guibord.tech>
2025-05-20 19:37:18 -0600

    Re-arranged nginx to start sooner to settle itself out

--- mass-start.bat
+++ mass-start.bat
@@ -2,9 +2,11 @@ docker compose --file ./Prod/Chrony/compose.yaml up --detach
 docker compose --file ./Prod/PostgreSQL/compose.yaml up --detach
 REM Sleeps for 20 seconds, lets Postgres sort itself out
 timeout /t 20
+docker compose --file ./Prod/Nginx-Proxy/compose.yaml up --detach
+REM Sleeps for 20 seconds, lets Nginx sort itself out
+timeout /t 20
 docker compose --file ./Prod/Apache/compose.yaml up --detach
 docker compose --file ./Prod/Pi-hole/compose.yaml up --detach
-docker compose --file ./Prod/Nginx-Proxy/compose.yaml up --detach
 docker compose --file ./Prod/NextCloud/compose.yaml up --detach
 docker compose --file ./Prod/Homer/compose.yaml up --detach
 docker compose --file ./Prod/Portainer/compose.yaml up --detach
